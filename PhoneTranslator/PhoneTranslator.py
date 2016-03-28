d5e779ec2b6fb0fa4f8929a1090bc9059ff08dcd
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-03-28 12:05:53 -0400

    Finished method

--- PhoneTranslator/PhoneTranslator.py
+++ PhoneTranslator/PhoneTranslator.py
@@ -1,3 +1,9 @@
 import itertools
 
-keypad = {1:"ABC", 2:"DEF", 3:"GHI", 4:"JKL", 5:"MNO", 6:"PQRS", 7:"TUV", 8:"WXYZ"}
+keypad = {2:"ABC", 3:"DEF", 4:"GHI", 5:"JKL", 6:"MNO", 7:"PQRS", 8:"TUV", 9:"WXYZ"}
+
+def translate(*code):
+    items = [keypad[key] for key in code]
+    result = (''.join(a) for a in itertools.product(*items))
+    result = filter(lambda s: any(v in s for v in "AEIOUY"), result)
+    return result;
